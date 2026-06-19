eabb943fabc856e20b1601c00004f1bbc54daae2
Chhatrapati sahu <chhatrapatisahu09@gmail.com>
2026-06-19 20:52:08 +0530

    Initial project setup with Java main class

---
+++ TypingSpeedTester.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class TypingSpeedTester {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Typing Speed Tester ===");
+
+    }
+}
